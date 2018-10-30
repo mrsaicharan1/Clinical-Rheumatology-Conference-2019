ba6ed15474ebf274d4f34ef4d0d64c319ac76372
Saicharan Reddy <saicharan.reddy1@gmail.com>
2018-10-31 01:40:19 +0530

    Added initial files

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# Secret key for session management. You can generate random strings here:
+# https://randomkeygen.com/
+SECRET_KEY = 'my precious'
+
+# Connect to the database
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
+
+#AWS credentials
+S3_BUCKET                 = "zappa-jepozm5pw"
+S3_KEY                    = "AKIAJPWGUHCTWRZ3GJYA"
+S3_SECRET                 = "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"
+S3_LOCATION               = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
+
+#Mail settings
---
+++ forms.py
@@ -0,0 +1,45 @@
+from flask_wtf import Form
+from wtforms import TextField, PasswordField, SelectField
+from wtforms.validators import DataRequired, EqualTo, Length
+
+# Set your classes here.
+
+
+class RegisterForm(Form):
+    name = TextField(
+        'Username', validators=[DataRequired(), Length(min=6, max=25)]
+    )
+    email = TextField(
+        'Email', validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    password = PasswordField(
+        'Password', validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    confirm = PasswordField(
+        'Repeat Password',
+        [DataRequired(),
+        EqualTo('password', message='Passwords must match')]
+    )
+    type = SelectField(
+        'Select Type',
+        choices=[('sub','Subscriber'),('rev','Reviewer'),('pub','publisher')]
+    )
+
+
+class LoginForm(Form):
+    email = TextField('Email', [DataRequired()])
+    password = PasswordField('Password', [DataRequired()])
+
+
+class ForgotForm(Form):
+    email = TextField(
+        'Email', validators=[DataRequired(), Length(min=6, max=40)]
+    )
+
+class ResetForm(Form):
+    password = TextField(
+        'Password', validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    repeat_password = TextField(
+        'Confirm-Password', validators=[DataRequired(), Length(min=6, max=40)]
+    )
